42e4765c05272b57834f4a4d6c86702547d74e51
AndyGIbarra <ibarraandy101@gmail.com>
2022-12-13 22:44:34 -0800

    Forgot form, added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 // Make sure to import the component we just built:
 import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
 
 function App(){
     let [colors, setColors] = useState([
@@ -13,10 +14,14 @@ function App(){
     let colorMap = colors.map((color, i) => {
         return <ColorBlock key={i} colorVal={color} />
     })
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
 
     return (
         <div className="App">
             {colorMap}
+            <ColorForm addColor={addColor} />
         </div>
     )
 }
